4fe36b75cbba3f606259c73addeef3035ef1f85c
Jam3e3333 <jamm3e3333@gmail.com>
2021-08-24 22:17:40 -0400

    creating a routings

---
+++ basic-routing/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+    return (
+        <div>
+            <h1>All events</h1>
+        </div>
+    )
+}
+
+export default FilteredEventsPage;
---
+++ basic-routing/pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEvents = () => {
+    return (
+        <div>
+            <h1>All events</h1>
+        </div>
+    )
+}
+
+export default AllEvents;
